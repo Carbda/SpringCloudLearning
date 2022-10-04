572ca1845ca32ce8162af343fbcf57def9fe44cd
wangyuanhao <740458268@qq.com>
2022-10-04 17:14:50 +0800

    Ribbon

--- ribbon-service/src/main/java/com/example/ribbonservice/config/RibbonConfig.java
+++ ribbon-service/src/main/java/com/example/ribbonservice/config/RibbonConfig.java
@@ -1,10 +1,13 @@
-package com.example.ribbonservice;
+package com.example.ribbonservice.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Created by macro on 2019/8/29.
+ */
 @Configuration
 public class RibbonConfig {
 
